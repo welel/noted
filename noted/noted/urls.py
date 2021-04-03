8d2d2fdd72a8cd16d6c2a31ec3b1acc759f5a1e5
Pavel Loginov <pavel.loginov.dev@gmail.com>
2021-04-04 00:16:50 +0300

    Updated general urls.py.

--- noted/noted/urls.py
+++ noted/noted/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('notes.urls')),
+    path('notes/', include('notes.urls')),
     path('accounts/', include('allauth.urls')),
 ]
