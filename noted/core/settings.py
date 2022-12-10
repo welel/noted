6d8b799c1fc184fc65fe4e2d536f4df6186cf061
Pavel Loginov <pavel.loginov.dev@gmail.com>
2022-12-10 20:55:31 +0300

    allow hosts, fix template

--- noted/core/settings.py
+++ noted/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
