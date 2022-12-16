8720425ce328b90d83ef930bb6bf1a97d7961ae2
Pavel Loginov <pavel.loginov.dev@gmail.com>
2022-12-16 18:44:51 +0300

    Updated bookmarks tabs.

--- frontend/static/js/common.js
+++ frontend/static/js/common.js
@@ -21,3 +21,16 @@ function bookmarkNote(note_pk) {
         timeout: 3000
     });
 }
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
