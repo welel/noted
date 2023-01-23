5b10db8a664d88dbcac99cbc30676d08f2abb842
Pavel Loginov <pavel.loginov.dev@gmail.com>
2023-01-24 00:15:19 +0300

    fix: added `.po` and `.mo` files back cause Docker can't compile `.po` files with translations.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,5 @@ sleep 5
 cd noted
 python manage.py makemigrate
 python manage.py collectstatic --noinput
-python noted/manage.py compilemessages -l ru
 gunicorn core.wsgi:application --bind 172.18.1.3:8000
 
