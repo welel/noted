456fad69f3cb7e9b6e4bf624e4d5546ba89d4dc8
Pavel Loginov <pavel.loginov.dev@gmail.com>
2023-02-13 11:42:15 +0300

    build: update celery settings.

--- noted/core/celery.py
+++ noted/core/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prodaction")
 
 app = Celery("core")
 
