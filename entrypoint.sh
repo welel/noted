92474bc25349a352b43c169e76ba6e2962006107
Eduard Antadze <113856407+eantdz@users.noreply.github.com>
2023-04-03 21:10:27 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ sleep 5
 cd noted
 python manage.py makemigrate --settings=core.settings.production
 python manage.py collectstatic --noinput --settings=core.settings.production
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --bind 0.0.0.0:10000
 
