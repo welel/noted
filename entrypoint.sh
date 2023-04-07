718de31a934f7b53f51054749866236c52705a31
Pavel Loginov <pavel.loginov.dev@gmail.com>
2023-04-07 23:23:42 +0300

    feat: populate preview text, image url in new DB columns;       calculate note's weight for sorting by relevance;       add new order method by relevance;

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
  
 sleep 5
 cd noted
-python manage.py makemigrate --settings=core.settings.production
+python manage.py migrate --settings=core.settings.production
 python manage.py collectstatic --noinput --settings=core.settings.production
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
