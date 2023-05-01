71cce956137cab29da1fde589ea4ca852601ea4d
Eduard Antadze <113856407+eantdz@users.noreply.github.com>
2023-05-01 14:58:38 +0300

    Update entrypoint.sh
    Add postgres wait instructions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
- 
-sleep 5
+#!/bin/bash
+
+# Wait for postgres 
+while ! nc -z $DATABASE_HOST $DATABASE_PORT; 
+  do sleep 0.1;
+done
+
+set -e
+# Running gunicorn
 cd noted
 python manage.py migrate --settings=core.settings.production
 python manage.py collectstatic --noinput --settings=core.settings.production
